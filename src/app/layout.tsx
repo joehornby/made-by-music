a64890b1dd519206c282e4c811dfad1c92124df9
Joe Hornby <joehornby@gmail.com>
2025-10-20 23:40:15 +0100

    feat: integrate Playback and Preload providers, and add AudioElement component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import Sidebar from "@/app/components/sidebar";
 import Topbar from "@/app/components/topbar";
 import Player from "@/app/components/player";
 import { DataProvider } from "@/app/contexts/data-context";
+import { PlaybackProvider } from "@/app/contexts/playback-context";
+import { PreloadProvider } from "@/app/contexts/preload-context";
+import AudioElement from "@/app/components/audio-element";
 
 const acidGrotesk = localFont({
   src: [
@@ -36,10 +39,15 @@ export default function RootLayout({
         className={`${acidGrotesk.variable} ${geistMono.variable} antialiased bg-dark text-light grid grid-cols-[96px_1fr] grid-rows-[72px_auto] p-4 gap-4`}
       >
         <DataProvider>
-          <Sidebar className="col-start-1 row-start-1" />
-          <Topbar className="col-start-2 col-span-1 row-start-1 sticky top-4" />
-          <div className="col-start-2 row-start-2 mb-48">{children}</div>
-          <Player className="fixed bottom-0 left-0" />
+          <PreloadProvider>
+            <PlaybackProvider>
+              <Sidebar className="col-start-1 row-start-1" />
+              <Topbar className="col-start-2 col-span-1 row-start-1 sticky top-4" />
+              <div className="col-start-2 row-start-2 mb-48">{children}</div>
+              <Player className="fixed bottom-0 left-0" />
+              <AudioElement />
+            </PlaybackProvider>
+          </PreloadProvider>
         </DataProvider>
       </body>
     </html>
